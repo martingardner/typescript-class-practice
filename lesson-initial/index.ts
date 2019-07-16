2aa826f2ed50ecbc392da0cc5b706565a18d3ba8
martin gardner II <martingardnerii@gmail.com>
2019-07-16 18:27:17 -0500

    Update lesson-initial to finish out the lesson, add in function typing

--- lesson-initial/index.ts
+++ lesson-initial/index.ts
@@ -15,9 +15,13 @@ axios.get(url).then(response => {
   const title = todo.title;
   const completed = todo.completed;
 
-  console.log(`
-        The Todo with ID: ${ID}
-        Has a title of: ${title}
-        Is it finished? ${completed}
-    `);
+  logTodo(ID, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+    `);
+};
