289063991f85b14b29e33079bc968cc60468250d
martin gardner II <martingardnerii@gmail.com>
2019-07-22 10:59:10 -0500

    type annotations vs type inference drills

--- lesson-typeannotations/variables.ts
+++ lesson-typeannotations/variables.ts
@@ -23,3 +23,37 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20
 };
+
+//Function
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+// -- Add object notation to define what is coming out of JSON.parse in order to define them.
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+// -- in this case foundWord will get an any type unless it's told it's a boolean before hand.
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // scenario, this variable could get either a number or if it doesn't match, a boolean
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
