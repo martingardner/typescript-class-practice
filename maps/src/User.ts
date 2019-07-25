bcc046705a6d356aeb5dc17bd2dcb2f43b679b04
martin gardner II <martingardnerii@gmail.com>
2019-07-25 14:32:34 -0500

    added in faker data for name and location lat lng values

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,11 +1,18 @@
 import faker from "faker";
 
 class User {
+  //typescript variable definitions
   name: string;
   location: {
     lat: number;
     lng: number;
   };
-
-  constructor() {}
+  // end typescript variable definitions
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
 }
